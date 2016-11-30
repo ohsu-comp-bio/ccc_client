f8230a6aecfe1f464e622a703b83a8f24e02d5c5
Alex Buchanan <buchanae@ohsu.edu>
2016-11-30 10:02:26 -0800

    Fix import statements for python 3

--- ccc_client/cli.py
+++ ccc_client/cli.py
@@ -8,10 +8,10 @@ import logging
 import sys
 
 import ccc_client
-import app_repo.cli
-import exec_engine.cli
-import dts.cli
-import dcs.cli
+import ccc_client.app_repo.cli
+import ccc_client.exec_engine.cli
+import ccc_client.dts.cli
+import ccc_client.dcs.cli
 
 try:
     import http.client as http_client
@@ -22,35 +22,35 @@ except ImportError:
 # Defines all the services and actions available in the CLI
 services = {
     'app-repo': {
-        'list-tools': app_repo.cli.list_tools,
-        'get-metadata': app_repo.cli.get_metadata,
-        'delete-metadata': app_repo.cli.delete_metadata,
-        'update-metadata': app_repo.cli.update_metadata,
-        'upload-metadata': app_repo.cli.upload_metadata,
-        'upload-image': app_repo.cli.upload_image,
+        'list-tools': ccc_client.app_repo.cli.list_tools,
+        'get-metadata': ccc_client.app_repo.cli.get_metadata,
+        'delete-metadata': ccc_client.app_repo.cli.delete_metadata,
+        'update-metadata': ccc_client.app_repo.cli.update_metadata,
+        'upload-metadata': ccc_client.app_repo.cli.upload_metadata,
+        'upload-image': ccc_client.app_repo.cli.upload_image,
     },
     'exec-engine': {
-        'submit': exec_engine.cli.submit,
-        'status': exec_engine.cli.status,
-        'metadata': exec_engine.cli.metadata,
-        'outputs': exec_engine.cli.outputs,
-        'query': exec_engine.cli.query,
+        'submit': ccc_client.exec_engine.cli.submit,
+        'status': ccc_client.exec_engine.cli.status,
+        'metadata': ccc_client.exec_engine.cli.metadata,
+        'outputs': ccc_client.exec_engine.cli.outputs,
+        'query': ccc_client.exec_engine.cli.query,
     },
     'dts': {
-        'post': dts.cli.post,
-        'put': dts.cli.put,
-        'get': dts.cli.get,
-        'delete': dts.cli.delete,
-        'infer-cccId': dts.cli.infer_cccid,
-        'query': dts.cli.query,
+        'post': ccc_client.dts.cli.post,
+        'put': ccc_client.dts.cli.put,
+        'get': ccc_client.dts.cli.get,
+        'delete': ccc_client.dts.cli.delete,
+        'infer-cccId': ccc_client.dts.cli.infer_cccid,
+        'query': ccc_client.dts.cli.query,
     },
     'dcs': {
-        'create-link': dcs.cli.create_link,
-        'find-common-sets': dcs.cli.find_common_sets,
-        'list-sets': dcs.cli.list_sets,
-        'list-resources': dcs.cli.list_resources,
-        'delete-link': dcs.cli.delete_link,
-        'delete-set': dcs.cli.delete_set,
+        'create-link': ccc_client.dcs.cli.create_link,
+        'find-common-sets': ccc_client.dcs.cli.find_common_sets,
+        'list-sets': ccc_client.dcs.cli.list_sets,
+        'list-resources': ccc_client.dcs.cli.list_resources,
+        'delete-link': ccc_client.dcs.cli.delete_link,
+        'delete-set': ccc_client.dcs.cli.delete_set,
     },
 }
 
